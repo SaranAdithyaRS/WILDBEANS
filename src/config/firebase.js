feaa5c893a087114ea2fc89d2bdc342750a2f688
Saran Adithya <saranadithya2407@gmail.com>
2025-05-16 18:39:38 +0530

    Update firebase.js

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyB1VLgX7izH2v54hUmtxKUEHW7PIdNlFlg",
-  authDomain: "wild-beans-fdb66.firebaseapp.com",
-  projectId: "wild-beans-fdb66",
-  storageBucket: "wild-beans-fdb66.firebasestorage.app",
-  messagingSenderId: "398120657852",
-  appId: "1:398120657852:web:65da3ea45f9c7fc8b3c1b9",
-  measurementId: "G-Z1JFKL28FC"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: ""
 };
 
 // Initialize Firebase
